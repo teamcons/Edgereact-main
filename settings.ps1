ba17a71fce95d9432486626909b4047238cc0ece
teamcons_atwork <littlepastelworld@gmail.com>
2024-10-17 17:34:14 +0200

    oops

--- settings.ps1
+++ settings.ps1
@@ -2,7 +2,7 @@
 
 # Loaded if present.
 
-[string]$hotcorner_where                = "topright"     # See README for options
+[string]$hotcorner_where                = "topleft"     # See README for options
 [string]$hotcorner_what                 = "overview"    # See README for options
 [int]$hotcorner_reactivity              = 400           # In milliseconds - How often to check mouse location.
 [byte]$hotcorner_sensitivity            = 30            # In pixels - Size of hot corner area
